8f1bbc8bf410b9110c4eb35cc2ab90c699eb339a
ericbsantana <bitencourteric@hotmail.com>
2022-02-21 22:35:22 -0300

    test: add password to requiredFields

--- src/presentation/controllers/SignUp/SignUp.spec.ts
+++ src/presentation/controllers/SignUp/SignUp.spec.ts
@@ -31,4 +31,19 @@ describe('Signup controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'guineapig',
+        email: 'guinea_pig@pigs.com',
+        passwordConfirmation: 'wheekwheek'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
