827960b7a18693cd08ffec96a33f55c1df7dc51e
ericbsantana <bitencourteric@hotmail.com>
2022-02-22 01:43:12 -0300

    test: ensure SignUpController return 200 if account is created

--- src/presentation/controllers/SignUp/SignUp.spec.ts
+++ src/presentation/controllers/SignUp/SignUp.spec.ts
@@ -215,4 +215,26 @@ describe('Signup controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'guineapig',
+        email: 'guinea_pig@pigs.com',
+        password: 'wheekwheek',
+        passwordConfirmation: 'wheekwheek'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    })
+  })
 })
